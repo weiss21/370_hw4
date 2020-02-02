851bb8e7c59357e761f9ee52061a279bc0bc443e
weiss21 <waisalirobleh@gmail.com>
2020-02-02 02:09:08 +0000

    work but format later

--- bfs.cpp
+++ bfs.cpp
@@ -5,57 +5,68 @@
  * ID: 1624
  * Date: 02/01/2020
  */
- 
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cmath>
 #include <vector>
+
 using namespace std;
 
-
-int main(){
-    
-    ifstream myFile;
-    string fileName;
-    int count;
-    int num, kNum;
-    
-   cout << "Enter input file name: ";
-   cin >> fileName;
-   myFile.open(fileName.c_str()); //open file
-   
-   if (myFile >> num)
+void insertInQueue(int queue[], int &length, int vertex)
+{
+    //cout << "insert " << vertex << endl;
+    for(int i = 0; i < length; i++)
     {
-      count = num; // first line details count of array
+        if(queue[i] == vertex)
+            return;
     }
-    //create matrix
-    int** dynamArray = new int*[count]; //initialize dynamic array
-    for(int k = 0; k < count; k++){
-        dynamArray[k] = new int[count];
+    queue[length] = vertex;
+    length++;
+
+}
+int main()
+
+{
+    string fileName;
+    int beginning;
+    cout << "Enter input file name: "; //asks input
+    cin >> fileName;
+    cout << "Enter a start vertex: ";
+    cin >> beginning;
+    ifstream infile(fileName.c_str());
+  
+    int N;
+    infile >> N; 
+
+    int adjacency[N][N];
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+            infile >> adjacency[i][j];
     }
+
+    infile.close();
     
-    for(int i = 0; i < count; i++){
-        for(int j = 0; j < count; j++){
-            myFile >> num;
-            dynamArray[i][j] = num;
+
+    int queue[N];
+    int queue_length = 0;
+    queue[0] = beginning;
+    queue_length++;
+    int vertex;
+    for(int i = 0; i < queue_length; i++)
+    {
+        vertex = queue[i];
+        for(int j = 0; j < N; j++)
+        {
+            if(adjacency[vertex][j] != 0)
+                insertInQueue(queue, queue_length, j);
         }
     }
-    
-    //prints out and tests for loop below
-    /*
-    for(int i = 0; i < count; i++){
-        for(int j = 0; j < count; j++){
-           cout << dynamArray[i][j] << " ";
-        }
-        cout << endl;
-    }
-    */
-    
-    //delete matrix
-    for(int k = 0; k < count; k++){
-        delete[] dynamArray[k];
-    }
-    delete[] dynamArray;
-    return 0;
+    cout << "\nBFS Order: ";
+    cout << queue[0] ;
+    for( int i = 1; i < queue_length; i++)
+        cout << " ---> " << queue[i] ;
+    cout << endl;
 }
